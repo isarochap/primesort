42f94a4e1f6f197f60a8acab85e423e4e827dced
Isabella Rocha <isa.rochap@gmail.com>
2017-11-29 20:33:10 -0200

    upload

--- src/main.c
+++ src/main.c
@@ -1,17 +1,44 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+
+#define MAX 200000
+
+int naoEhPrimo(int numero) {
+
+	if (numero <= 1) return 1;
+
+	for(int i=2; i*i <= numero; i++){
+		if (numero % i == 0) return 1;
+	}
+	
+	return 0;
+}
 
 int main() {
 
-  int x, y;
+	int n, naoPrimos[MAX], nNaoPrimos = 0;
+	
+	do {
+		scanf("%d", &n);
+		if ((n>0) && naoEhPrimo(n)) {
+			naoPrimos[nNaoPrimos] = n;
+			nNaoPrimos++;
+		}
+	} while (n > 0);
+	
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	for(int i=0; i < nNaoPrimos; i++) {
+       for(int j=1; j < nNaoPrimos-i; j++) {
+           if (naoPrimos[j - 1] < naoPrimos[j]) {
+               int tmp = naoPrimos[j];
+               naoPrimos[j] = naoPrimos[j - 1];
+               naoPrimos[j - 1] = tmp;
+           }
+        }
+ 	}
+	
+	for(int i=0; i < nNaoPrimos; i++) {
+		printf("%d\n", naoPrimos[i]);
+	}
+
+    return 0;
 }
